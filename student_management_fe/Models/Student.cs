f3aa4491f0d3f6a26b5c9b2bfd69d28e1a312ee4
MaThanhNhi <mathanhnhi2004@gmail.com>
2025-03-10 18:41:09 +0700

    Follow MVC

---
+++ student_management_fe/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace student_management_fe.Models
+{
+    public class StudentModel
+    {
+        public string StudentId { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Faculty { get; set; }
+        public string Course { get; set; }
+        public string Program { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+    }
+}
--- student_management_fe/Pages/AddStudent.razor.cs
+++ student_management_fe/Pages/AddStudent.razor.cs
@@ -1,1 +1,19 @@
-﻿namespace student_management_fe.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using student_management_fe.Models;
+namespace student_management_fe.Pages;
+
+public partial class AddStudent
+{
+    private StudentModel Student = new StudentModel();
+    [Inject] private NavigationManager Navigation { get; set; }
+
+    private void CloseOverlay()
+    {
+        Navigation.NavigateTo("/", forceLoad: false);
+    }
+
+    private void Add()
+    {
+
+    }
+}
