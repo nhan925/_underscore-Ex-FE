4bc2eaca02d2c945c45b3036ca2f7f9388819944
MinhNguyenCG <info.nguyen1104@gmail.com>
2025-03-10 20:38:17 +0700

    Add login ui and dashboard ui

---
+++ student_management_fe/Models/Student.cs
@@ -0,0 +1,37 @@
+﻿
+namespace student_management_fe.Models
+{
+    public class StudentModel
+    {
+        public string StudentId { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string Faculty { get; set; }
+        public string IntakeYear { get; set; }
+        public string Program { get; set; }
+        public string Address { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Status { get; set; }
+
+        public StudentModel()
+        {
+
+        }
+        public StudentModel(StudentModel s)
+        {
+            StudentId = s.StudentId;
+            FullName = s.FullName;
+            BirthDate = s.BirthDate;
+            Gender = s.Gender;
+            Faculty = s.Faculty;
+            IntakeYear = s.IntakeYear;
+            Program = s.Program;
+            Address = s.Address;
+            Email = s.Email;
+            PhoneNumber = s.PhoneNumber;
+            Status = s.Status;
+        }
+    }
+}
--- student_management_fe/Program.cs
+++ student_management_fe/Program.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace student_management_fe;
@@ -10,7 +10,7 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
-
+        // Thêm backend url ở đây
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
         await builder.Build().RunAsync();
