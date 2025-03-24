86bf08a9ac01ac61c8e2030eeded343921cfabb2
MinhNguyenCG <info.nguyen1104@gmail.com>
2025-03-24 19:53:24 +0700

    Add Email Setting, Phone Number Setting and API

---
+++ student_management_fe/Models/CountryPhoneCodeModel.cs
@@ -0,0 +1,8 @@
+﻿namespace student_management_fe.Models;
+
+public class CountryPhoneCodeModel
+{
+    public string Name { get; set; } = string.Empty;
+    public string Code { get; set; } = string.Empty;
+    public string CallingCode { get; set; } = string.Empty;
+}
